e32af42b4a2aaa371e94ccb26120c97437831188
Heimlich <jerry.lin@tradevan.com.tw>
2021-10-04 16:39:05 +0800

    to git

---
+++ src/main/java/idv/heimlich/Spring5/App.java
@@ -0,0 +1,14 @@
+package idv.heimlich.Spring5;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+	private final static Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOG.info("Hello World!");
+		System.out.println("Hello World!");
+	}
+}
